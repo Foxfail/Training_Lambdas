a6b35be2b0d64ca276d7e0adab16b21b005f9d1f
Foxfail <foxfail@mail.ru>
2020-01-13 18:21:36 +0300

    Примеры использования лямбда выражений для функциональных интерфейсов с одним параметром

--- src/com/foxfail/Main.java
+++ src/com/foxfail/Main.java
@@ -1,8 +1,24 @@
 package com.foxfail;
 
+import com.foxfail.interfaces.Functional;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // Реализация метода функционального интерфейса совпадает с методом println
+        acceptFunctional(System.out::println);
+
+
+        // Реализация метода представляет собой две команды
+        acceptFunctional( s -> {
+            s += "2";
+            System.out.println(s);
+        });
+
+    }
+
+    private static void acceptFunctional(Functional f){
+        // в реализацию метода function1 отправляем тестовую строку
+        f.function1("testString");
     }
 }
