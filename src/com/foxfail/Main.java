8db846e01cc7a91ff20f88aa6f27801762b1a170
Foxfail <foxfail@mail.ru>
2020-01-13 18:50:35 +0300

    Реализуем метод функционального интерфейса который возвращает Integer

--- src/com/foxfail/Main.java
+++ src/com/foxfail/Main.java
@@ -1,6 +1,7 @@
 package com.foxfail;
 
 import com.foxfail.interfaces.Functional;
+import com.foxfail.interfaces.FunctionalWithReturnValue;
 
 public class Main {
 
@@ -8,17 +9,40 @@ public class Main {
         // Реализация метода функционального интерфейса совпадает с методом println
         acceptFunctional(System.out::println);
 
-
-        // Реализация метода представляет собой две команды
-        acceptFunctional( s -> {
+        // Реализация метода функционального интерфейса представляет собой две команды
+        acceptFunctional(s -> {
             s += "2";
             System.out.println(s);
         });
 
+
+
+        // Реализуем метод функционального интерфейса который возвращает Integer
+        // Использую излишнюю разметку для наглядности
+        acceptFunctionalWithReturnValue((String s) -> {
+            if (s != null) {
+                return s.length();
+            } else {
+                return 0;
+            }
+        });
+
+        // Тоже самое только в сокращенном виде и с тернарным оператором
+        acceptFunctionalWithReturnValue(s -> s != null ? s.length() : 0);
+
+        // Без проверки на null используя method reference
+        acceptFunctionalWithReturnValue(String::length);
+
     }
 
-    private static void acceptFunctional(Functional f){
+    private static void acceptFunctional(Functional f) {
         // в реализацию метода function1 отправляем тестовую строку
         f.function1("testString");
+    }
+
+    private static void acceptFunctionalWithReturnValue(FunctionalWithReturnValue f) {
+        // в реализацию метода function1 отправляем тестовую строку
+        Integer i = f.function2("testString3");
+        System.out.println(i.toString());
     }
 }
